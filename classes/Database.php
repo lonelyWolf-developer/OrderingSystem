94ab08f7220c06bd9791e9761f7d2d3c8ab036ab
lonelyWolf <spravce@vlksamotar.cz>
2024-08-25 10:41:00 +0200

    Change database conection.

--- classes/Database.php
+++ classes/Database.php
@@ -3,15 +3,15 @@
 class Database{
 
     public function connectionDB(){
-            $db_host = "127.0.0.1";
-            $db_user = "vlk";
-            $db_password = "Admin123";
-            $db_name = "orders";
+            // $db_host = "127.0.0.1";
+            // $db_user = "vlk";
+            // $db_password = "Admin123";
+            // $db_name = "orders";
 
-            // $db_host = "sql6.webzdarma.cz";
-            // $db_user = "vlksamotarwz2218";
-            // $db_password = "H:3;=EICr16jx;r@Q!xz";
-            // $db_name = "vlksamotarwz2218";
+            $db_host = "sql6.webzdarma.cz";
+            $db_user = "vlksamotarwz2218";
+            $db_password = "H:3;=EICr16jx;r@Q!xz";
+            $db_name = "vlksamotarwz2218";
 
             $connection = "mysql:host=" . $db_host . ";dbname=" . $db_name . ";charset=utf8";
 
