2707a779acf597fa49c5997390cd296ef362dd62
lonelyWolf <spravce@vlksamotar.cz>
2024-08-25 09:47:21 +0200

    Adding responsivity to writeOrders.php file.

--- assets/writeOrders.php
+++ assets/writeOrders.php
@@ -26,35 +26,41 @@
             ?>
 
             <div class="row <?= $contract->type ?>">
-                <div class="contractInfo"><?= htmlspecialchars($contract->orderNumber) ?></div>
-                <div class="contractInfo"><?= htmlspecialchars($contract->type) ?></div>
-                <div class="contractInfo"><?= htmlspecialchars($contract->date) ?></div>
-                <div class="contractInfo"><?= htmlspecialchars($contract->time) ?></div>
-                <div class="contractInfo"><?= htmlspecialchars(explode('@', $contractUser->email)[0] ) ?></div>
-                <?php if($contract->changingUser != null and $contract->status != 0): ?>
-                    <div class="contractInfo"><?= htmlspecialchars(explode('@', $changeUser->email)[0] ) ?></div>
-                <?php endif; ?>                            
-                <?php if($contract->status == 0): ?>
-                    <div class="contractInfo">                          
-                        <?php if (Auth::checkRole(Roles::Warehouseman->value)): ?>
-                            <form action="./admin/changeContractStatus.php" method="post">
-                                <input type="hidden" name="Id" value="<?= $contract->id ?>">
-                                <input type="hidden" name="Status" value="<?= ContractStatus::Retrieved->value ?>">
-                                <input type="hidden" name="ChangingUser" value="<?= $user->id ?>">
-                                <input type="hidden" name="ReturnQuery" value="<?= $encodeQuery ?>">
-                                <input type="submit" value="Vyskladněno" id="deleteContractButton" class="deleteSubmit">
-                            </form>
-                        <?php elseif(Auth::checkRole(Roles::Admin->value) or Auth::checkRole(Roles::Craftsman->value)): ?>
-                            <form action="./admin/changeContractStatus.php" method="post">
-                                <input type="hidden" name="Id" value="<?= $contract->id ?>">
-                                <input type="hidden" name="Status" value="<?= ContractStatus::Cancelled->value ?>">
-                                <input type="hidden" name="ChangingUser" value="<?= $user->id ?>">
-                                <input type="hidden" name="ReturnQuery" value="<?= $encodeQuery ?>">
-                                <input type="submit" value="Zrušit" id="deleteContractButton" class="deleteSubmit">
-                            </form>
-                        <?php endif; ?>                
-                    </div>
-                <?php endif; ?>
+                <div class="smallRow">
+                    <div class="contractInfo"><?= htmlspecialchars($contract->orderNumber) ?></div>
+                    <div class="contractInfo"><?= htmlspecialchars($contract->type) ?></div>
+                </div>
+                <div class="smallRow">
+                    <div class="contractInfo"><?= htmlspecialchars($contract->date) ?></div>
+                    <div class="contractInfo"><?= htmlspecialchars($contract->time) ?></div>
+                </div>
+                <div class="smallRow">
+                    <div class="contractInfo"><?= htmlspecialchars(explode('@', $contractUser->email)[0] ) ?></div>
+                    <?php if($contract->changingUser != null and $contract->status != 0): ?>
+                        <div class="contractInfo"><?= htmlspecialchars(explode('@', $changeUser->email)[0] ) ?></div>
+                    <?php endif; ?>                            
+                    <?php if($contract->status == 0): ?>
+                        <div class="contractInfo">                          
+                            <?php if (Auth::checkRole(Roles::Warehouseman->value)): ?>
+                                <form action="./admin/changeContractStatus.php" method="post">
+                                    <input type="hidden" name="Id" value="<?= $contract->id ?>">
+                                    <input type="hidden" name="Status" value="<?= ContractStatus::Retrieved->value ?>">
+                                    <input type="hidden" name="ChangingUser" value="<?= $user->id ?>">
+                                    <input type="hidden" name="ReturnQuery" value="<?= $encodeQuery ?>">
+                                    <input type="submit" value="Vyskladněno" id="deleteContractButton" class="deleteSubmit">
+                                </form>
+                            <?php elseif(Auth::checkRole(Roles::Admin->value) or Auth::checkRole(Roles::Craftsman->value)): ?>
+                                <form action="./admin/changeContractStatus.php" method="post">
+                                    <input type="hidden" name="Id" value="<?= $contract->id ?>">
+                                    <input type="hidden" name="Status" value="<?= ContractStatus::Cancelled->value ?>">
+                                    <input type="hidden" name="ChangingUser" value="<?= $user->id ?>">
+                                    <input type="hidden" name="ReturnQuery" value="<?= $encodeQuery ?>">
+                                    <input type="submit" value="Zrušit" id="deleteContractButton" class="deleteSubmit">
+                                </form>
+                            <?php endif; ?>                
+                        </div>
+                    <?php endif; ?>  
+                </div>
             </div>
         <?php endforeach ?>
     <?php else: ?>
